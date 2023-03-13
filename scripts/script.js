d9efc9588cf8649133b1f8c9b36226b4c10b74bc
DanielKriegerS <danielschifelbain@gmail.com>
2023-03-13 14:00:34 -0300

    Adjusted responsivity (with GRID bootstrap)

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 function switchStyle() {
-    var body = document.getElementById("body");
-    var btn = document.getElementById("btn");
+  var body = document.getElementById("body");
+  var btn = document.getElementById("btn");
 
-    if (body.classList.contains("darkBackgroundStyle")){
-        body.classList.remove("darkBackgroundStyle");
-        btn.classList.remove("darkBackgroundStyle");
-        btn.textContent = "DARK MODE";    
-    } else {
-        body.classList.add("darkBackgroundStyle");
-        btn.classList.add("darkBackgroundStyle");
-        btn.textContent = "LIGHT MODE";
-    }
+  if (body.classList.contains("darkBackgroundStyle")) {
+    body.classList.remove("darkBackgroundStyle");
+    btn.classList.remove("darkBackgroundStyle");
+    btn.textContent = "DARK MODE";
+  } else {
+    body.classList.add("darkBackgroundStyle");
+    btn.classList.add("darkBackgroundStyle");
+    btn.textContent = "LIGHT MODE";
   }
+}
